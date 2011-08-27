e345ce880d242f2b8116ed64658ec69847cd6f4a
Bill Curry <bill@curry.name>
2011-08-27 19:49:21 -0400

    Trimmed down things I don't need/use; Added Compass support; Added CoffeeScript support

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
-/* Author:
-
-*/
-
-
-
-
-
+(function() {
+  var site, util;
+  site = {
+    common: {
+      init: function() {}
+    },
+    main: {
+      init: function() {},
+      home: function() {}
+    }
+  };
+  util = {
+    exec: function(controller, action) {
+      var _base;
+      if (action == null) {
+        action = 'init';
+      }
+      if (site[controller]) {
+        return typeof (_base = site[controller])[action] === "function" ? _base[action]() : void 0;
+      }
+    },
+    init: function() {
+      var action, body, controller;
+      body = document.body;
+      controller = body.getAttribute('data-controller');
+      action = body.getAttribute('data-action');
+      util.exec('common');
+      util.exec(controller);
+      return util.exec(controller, action);
+    }
+  };
+  $(document).ready(util.init);
+}).call(this);
